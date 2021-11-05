386c7538599e64dc1b5a9739c8be7e0293734a16
Evandro Oliveira <evandro.ricardo@hotmail.com>
2021-11-04 22:33:07 -0300

    teste com post

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,8 @@
 module.exports = app => {
-    app.get ('/atendimentos', (req, res) => res.send('Rota de atendimentos'))
+    app.get ('/atendimentos', (req, res) => res.send('Rota de atendimentos get !!'))
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('REQUISIÇÃO POST !!!!')
+    })
 }
